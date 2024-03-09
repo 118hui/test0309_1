1294eb81353b0d621634798b4cb04380aaa0e326
李盈輝 <109151149+118hui@users.noreply.github.com>
2024-03-09 18:17:22 +0800

    version1

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import HelloWorld from "./components/HelloWorld.vue";
+import Navbar from "./components/Navbar.vue";
+import Footer from "./components/Footer.vue";
+import About from "./components/About.vue";
+import Cta from "./components/Cta.vue";
+import Contact from "./components/Contact.vue";
+import Card from "./components/Card.vue";
+import Four from "./components/Four.vue";
+</script>
+
+<template>
+  <Navbar />
+  <HelloWorld />
+  <About />
+  <Cta />
+  <Contact />
+  <Footer />
+  <Card />
+  <Four />
+</template>
+
+<style scoped></style>
---
+++ src/components/Card.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="text-5xl font-bold">就讓這一首歌</div>
+</template>
+
+<script></script>
